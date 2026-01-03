9092dabcfb4513694df509d83842cfc9ae6a7359
kristian wiklund <githubkristian@snabela.nl>
2026-01-03 17:22:17 +0100

    displaying a sprite

--- src/main.c
+++ src/main.c
@@ -1,23 +1,31 @@
 #include <c64.h>
 #include <peekpoke.h>
 
-#https://cc65.github.io/mailarchive/2008-03/6026.html
+// https://cc65.github.io/mailarchive/2008-03/6026.html
 
-void music_init(void) {
-}
-
-
-void music_play(void) {
-}
-
+void my_irq_2(void);
 
 void my_irq(void) {
-  ++VIC.bgcolor0;
-  music_play();
-  --VIC.bgcolor0;
+
+    VIC.bordercolor=COLOR_YELLOW;
+    VIC.rasterline = 255;
+    VIC.ctrl1&=0xf7;
 
   /* ack raster IRQ */
   VIC.irr = 1;
+   POKEW(0x0314, (int)&my_irq_2);
+
+  __asm__(" jmp $ea31");
+}
+
+void my_irq_2(void) {
+    VIC.bordercolor=COLOR_BLACK;
+    VIC.rasterline = 249;
+    VIC.ctrl1 |=0x08;
+  /* ack raster IRQ */
+  VIC.irr = 1;
+   POKEW(0x0314, (int)&my_irq);
+
   __asm__(" jmp $ea31");
 }
 
@@ -27,8 +35,7 @@ void __fastcall__ irq_setup(void (*irqh)(void)) {
   CIA1.icr = 0x7f;
   VIC.imr = 0;
 
-  /* irq on line 100 */
-  VIC.rasterline = 100;
+  VIC.rasterline = 249;
   VIC.ctrl1 = 0x1b;
 
   /* set kernal IRQ vector */
@@ -38,11 +45,23 @@ void __fastcall__ irq_setup(void (*irqh)(void)) {
   VIC.imr = 1;
 }
 
+extern char sprite1[];
+char *smem=(char *)832;
 
 void main(void) {
-  music_init();
-
+    unsigned char n;
   irq_setup(&my_irq);
+  VIC.spr_ena=1;
+  VIC.spr0_x=50;
+  VIC.spr0_y=50;
+  POKE(2040,13);
+
+  // https://www.commodore.ca/manuals/c64_users_guide/c64-users_guide-06-sprite_graphics.pdf
+
+
+for (n = 0 ; n <= 62; n++) {
+ 		POKE(832+n,sprite1[n]);
+    }
 
   while (PEEK(0xc6) == 0) {
     ;
---
+++ src/sprites.s
@@ -0,0 +1,15 @@
+; // 1 sprites generated with spritemate on 2026-01-03, 16:46:00
+; // Byte 64 of each sprite contains multicolor (high nibble) & color (low nibble) information
+
+; // sprite 1 / singlecolor / color: $04
+.export _sprite1
+
+_sprite1:
+.byte $00,$00,$00,$00,$00,$00,$1e,$00
+.byte $38,$23,$00,$6c,$23,$01,$84,$21
+.byte $01,$0c,$11,$ff,$18,$10,$00,$30
+.byte $10,$00,$18,$23,$c3,$8c,$23,$22
+.byte $c4,$23,$e3,$84,$20,$c0,$0c,$26
+.byte $00,$08,$27,$17,$18,$31,$ff,$b0
+.byte $1c,$20,$60,$06,$00,$c0,$01,$ff
+.byte $80,$00,$00,$00,$00,$00,$00,$04
