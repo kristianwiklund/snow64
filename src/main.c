d053c4cc3c3b876e033732532c72e6903450747d
kristian wiklund <githubkristian@snabela.nl>
2026-01-04 11:33:35 +0100

    moving sprites

--- src/main.c
+++ src/main.c
@@ -3,13 +3,30 @@
 
 // https://cc65.github.io/mailarchive/2008-03/6026.html
 
+unsigned char sprite_y=50;
+unsigned int sprite_x=300;
+
 void my_irq_2(void);
+
+// place a sprite on the screen
+void position_sprite(unsigned char nr, unsigned int x, unsigned char y) {
+
+    VIC.spr_pos[nr].y = y;
+    VIC.spr_pos[nr].x = (unsigned char)(x&0xff);
+
+    // magic magix
+    if (x&0x100) 
+        VIC.spr_hi_x|=(1<<nr);
+    else
+        VIC.spr_hi_x&=(0xff ^ (1<<nr));
+    
+}
 
 void my_irq(void) {
 
     VIC.bordercolor=COLOR_YELLOW;
     VIC.rasterline = 255;
-    VIC.ctrl1&=0xf7;
+   // VIC.ctrl1&=0xf7;
 
   /* ack raster IRQ */
   VIC.irr = 1;
@@ -21,10 +38,16 @@ void my_irq(void) {
 void my_irq_2(void) {
     VIC.bordercolor=COLOR_BLACK;
     VIC.rasterline = 249;
-    VIC.ctrl1 |=0x08;
+  //  VIC.ctrl1 |=0x08;
   /* ack raster IRQ */
   VIC.irr = 1;
    POKEW(0x0314, (int)&my_irq);
+
+   sprite_y++;
+   sprite_x+=2;
+
+    position_sprite(0,sprite_x,sprite_y);
+
 
   __asm__(" jmp $ea31");
 }
@@ -52,8 +75,10 @@ void main(void) {
     unsigned char n;
   irq_setup(&my_irq);
   VIC.spr_ena=1;
-  VIC.spr0_x=50;
-  VIC.spr0_y=50;
+//   VIC.spr0_y=sprite_y;
+//   VIC.spr0_x=sprite_x;
+
+
   POKE(2040,13);
 
   // https://www.commodore.ca/manuals/c64_users_guide/c64-users_guide-06-sprite_graphics.pdf
