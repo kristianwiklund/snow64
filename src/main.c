dea89338711996759e73851224721ec93b1996cf
kristian wiklund <githubkristian@snabela.nl>
2026-01-04 11:53:29 +0100

    changed sprite placement to inline

--- src/main.c
+++ src/main.c
@@ -8,45 +8,60 @@ unsigned int sprite_x=300;
 
 void my_irq_2(void);
 
-// place a sprite on the screen
-void position_sprite(unsigned char nr, unsigned int x, unsigned char y) {
+// // place a sprite on the screen
+// this is superslow due to the function call!
+// void __fastcall__ position_sprite(unsigned char nr, unsigned int x, unsigned char y) {
 
-    VIC.spr_pos[nr].y = y;
-    VIC.spr_pos[nr].x = (unsigned char)(x&0xff);
+//     VIC.spr_pos[nr].y = y;
+//     VIC.spr_pos[nr].x = (unsigned char)(x&0xff);
 
-    // magic magix
-    if (x&0x100) 
-        VIC.spr_hi_x|=(1<<nr);
-    else
-        VIC.spr_hi_x&=(0xff ^ (1<<nr));
+//     // magic magix
+//     if (x&0x100) 
+//         VIC.spr_hi_x|=(1<<nr);
+//     else
+//         VIC.spr_hi_x&=(0xff ^ (1<<nr));
     
-}
+// }
+
+// macro defined place sprite on screen. Uses about half a raster line
+#define position_sprite_m(nr,xxx,yyy) {VIC.spr_pos[nr].y = (yyy);\
+    VIC.spr_pos[nr].x = (unsigned char)((xxx)&0xff);\
+    if ((xxx)&0x100) \
+        VIC.spr_hi_x|=(1<<nr);\
+    else\
+        VIC.spr_hi_x&=(0xff ^ (1<<nr));}
+
+// interrupt processing. Yellow bar shows the used time
 
 void my_irq(void) {
 
     VIC.bordercolor=COLOR_YELLOW;
-    VIC.rasterline = 255;
-   // VIC.ctrl1&=0xf7;
+   // VIC.rasterline = 255;
+    // VIC.ctrl1&=0xf7;
 
-  /* ack raster IRQ */
-  VIC.irr = 1;
-   POKEW(0x0314, (int)&my_irq_2);
+    /* ack raster IRQ */
+    VIC.irr = 1;
+    //  POKEW(0x0314, (int)&my_irq_2);
+
+    sprite_y++;
+    sprite_x+=2;
+
+    // an exceptionally inefficient routine
+    position_sprite_m(0,sprite_x,sprite_y);
+    VIC.bordercolor=COLOR_BLACK;
+ 
 
   __asm__(" jmp $ea31");
 }
 
+// used for border removal - not active now
 void my_irq_2(void) {
-    VIC.bordercolor=COLOR_BLACK;
     VIC.rasterline = 249;
   //  VIC.ctrl1 |=0x08;
   /* ack raster IRQ */
   VIC.irr = 1;
    POKEW(0x0314, (int)&my_irq);
 
-   sprite_y++;
-   sprite_x+=2;
-
-    position_sprite(0,sprite_x,sprite_y);
 
 
   __asm__(" jmp $ea31");
